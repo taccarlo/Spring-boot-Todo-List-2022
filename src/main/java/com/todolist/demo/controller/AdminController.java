70e05e29b6cc0ecb1e89e50b140fe4333b4890a8
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-09-09 15:45:27 +0200

    [FIX] problem with UX/UI

---
+++ src/main/java/com/todolist/demo/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.todolist.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+}
+
--- src/main/java/com/todolist/demo/controller/HomeController.java
+++ src/main/java/com/todolist/demo/controller/HomeController.java
@@ -4,6 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * This class manages the views navigation of the User
+ * For this class are needed to add the Starters: [Thymeleaf]
+ * <ul>
+ * <li>Controller: The Controller manage the views navigation.</li>
+ * <li>RequestMapping: Receives in input the page where to navigate.</li>
+ * <li>ResponseBody: indicates that the input is directly the html text to render on the view.</li>
+ * </ul>
+ * 
+ * @version 0.0.1
+ * @author Carlo Tacchella
+ * @since 22/09/07
+ */
+
 @Controller
 public class HomeController {
 
